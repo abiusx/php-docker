a39a499afb521b19ea78603a4be80541233df4a2
AbiusX <git@abiusx.com>
2018-05-22 01:19:22 -0400

    docker-compose version 2 syntax, better index.php example, fixed support for redis and memcached

--- www/index.php
+++ www/index.php
@@ -1,5 +1,19 @@
 <?php
+$mem_var = new Memcached();
+$mem_var->addServer("memcached", 11211);
+// for ($i=0;$i<10000;++$i)
+	$mem_var->get("test");
+
+
+$redisClient = new Redis();
+$redisClient->connect( 'redis' );
+// for ($i=0;$i<10000;++$i)
+	$redisClient->get("test");
+	// $redisClient -> set('key',"test".$redisClient -> get('key'));
+
+
 $con=new \PDO("mysql:host=mysql","root","123456aB");//
 $con->exec("CREATE DATABASE test");
 echo `whoami`;
 phpinfo();
+
